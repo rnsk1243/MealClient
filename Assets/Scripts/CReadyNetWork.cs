dab6f9dabaa2e732b374dea3052333ac01f91141
rnsk1243 <rnsk8945@gmail.com>
2017-09-08 03:48:44 +0900

    Sendn, Listener 까지

---
+++ Assets/Scripts/CReadyNetWork.cs
@@ -0,0 +1,186 @@
+﻿using System.Runtime.InteropServices;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ConstValueInfo;
+
+
+
+//[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+//public struct DataPacket
+//{
+//    [MarshalAs(UnmanagedType.I4)]
+//    public int Grade;
+
+//    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 1024)]
+//    public string Memo;
+
+//    // Calling this method will return a byte array with the contents
+//    // of the struct ready to be sent via the tcp socket.
+//    public byte[] Serialize()
+//    {
+//        // allocate a byte array for the struct data
+//        var buffer = new byte[Marshal.SizeOf(typeof(DataPacket))];
+
+//        // Allocate a GCHandle and get the array pointer
+//        var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+//        var pBuffer = gch.AddrOfPinnedObject();
+
+//        // copy data from struct to array and unpin the gc pointer
+//        Marshal.StructureToPtr(this, pBuffer, false);
+//        gch.Free();
+
+//        return buffer;
+//    }
+
+//    // this method will deserialize a byte array into the struct.
+//    public void Deserialize(ref byte[] data)
+//    {
+//        var gch = GCHandle.Alloc(data, GCHandleType.Pinned);
+//        this = (DataPacket)Marshal.PtrToStructure(gch.AddrOfPinnedObject(), typeof(DataPacket));
+//        gch.Free();
+//    }
+//}
+
+//public class CReadyNetWork {
+
+//    int Grade = 77;
+//    string Memo = "니미";
+
+//    public DataPacket func()
+//    {
+//        // 2. 구조체 데이타를 바이트 배열로 변환
+//        DataPacket packet = new DataPacket();
+//        //packet.Name = Name;
+//        //packet.Subject = Subject;
+//        packet.Grade = Grade;
+//        packet.Memo = Memo;
+
+//        byte[] buffer = packet.Serialize();
+
+//        // 3. 서버에 접속
+//        TcpClient client = new TcpClient();
+//        client.Connect("127.0.0.1", 9000);
+
+//        NetworkStream stream = client.GetStream();
+
+//        // 4. 데이타 전송
+//        stream.Write(buffer, 0, buffer.Length);
+
+//        byte[] buffer2 = new byte[1024];
+//        stream.Read(buffer2, 0, 1024);
+
+//        DataPacket packet2 = new DataPacket();
+//        packet2.Deserialize(ref buffer2);
+
+//        // 5. 스트림&소켓 닫기
+//        stream.Close();
+//        client.Close();
+
+//        return packet2;
+//    }
+
+//}
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+public struct DataPacketInt
+{
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = ConstVaue.IntSize)]
+    public int Number;
+
+    public DataPacketInt(int number)
+    {
+        Number = number;
+    }
+
+    // Calling this method will return a byte array with the contents
+    // of the struct ready to be sent via the tcp socket.
+    //public byte[] Serialize()
+    //{
+    //    // allocate a byte array for the struct data
+    //    var buffer = new byte[Marshal.SizeOf(typeof(DataPacketInt))];
+
+    //    // Allocate a GCHandle and get the array pointer
+    //    var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+    //    var pBuffer = gch.AddrOfPinnedObject();
+
+    //    // copy data from struct to array and unpin the gc pointer
+    //    Marshal.StructureToPtr(this, pBuffer, false);
+    //    gch.Free();
+
+    //    return buffer;
+    //}
+}
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+public struct DataPacketString
+{
+    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = ConstVaue.BufSize)]
+    public string Message;
+
+    public DataPacketString(string message)
+    {
+        Message = message;
+    }
+
+    public int GetStrLen()
+    {
+        return Message.Length;
+    }
+
+    // Calling this method will return a byte array with the contents
+    // of the struct ready to be sent via the tcp socket.
+    //public byte[] Serialize()
+    //{
+    //    // allocate a byte array for the struct data
+    //    var buffer = new byte[Marshal.SizeOf(typeof(DataPacketString))];
+
+    //    // Allocate a GCHandle and get the array pointer
+    //    var gch = GCHandle.Alloc(buffer, GCHandleType.Pinned);
+    //    var pBuffer = gch.AddrOfPinnedObject();
+
+    //    // copy data from struct to array and unpin the gc pointer
+    //    Marshal.StructureToPtr(this, pBuffer, false);
+    //    gch.Free();
+
+    //    return buffer;
+    //}
+}
+
+public class CReadyNetWork
+{
+    private static CReadyNetWork mInstance;
+    private TcpClient mClient;// = new TcpClient();
+    private NetworkStream mStream;
+
+    private CReadyNetWork()
+    {
+        mClient = new TcpClient();
+        mClient.Connect(ConstVaue.IP, ConstVaue.Port);
+        mStream = mClient.GetStream();
+    }
+    ~CReadyNetWork()
+    {
+        mStream.Close();
+        mClient.Close();
+    }
+    public static CReadyNetWork GetInstance()
+    {
+        if(null == mInstance)
+        {
+            mInstance = new CReadyNetWork();
+        }
+        return mInstance;
+    }
+    
+    public NetworkStream GetStream()
+    {
+        return mStream;
+    }
+
+}
+
---
+++ Assets/Scripts/ConstValueInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ConstValueInfo
+{
+    public class ConstVaue
+    {
+        public const int Port = 9000;
+        public const string IP = "127.0.0.1";
+        public const int BufSize = 1024;
+        public const int IntSize = 4;
+    }
+}
+
+
+
